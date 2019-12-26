ff1eed55c5e571cae8faf10e6faa14c8f67ba6c1
Asif Hasan <d.asif.hasan@gmail.com>
2019-12-25 19:47:25 -0600

    Removes code that serves static files from public folder.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import express from "express";
-import bodyParser from "body-parser";
-import path from "path";
-import mongoose from "mongoose";
-import bluebird from "bluebird";
-import { MONGODB_URI} from "./util/secrets";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import bluebird from 'bluebird';
+import { MONGODB_URI } from './util/secrets';
 
 // Controllers (route handlers)
-import * as homeController from "./controllers/home";
+import * as homeController from './controllers/home';
 
 // Create Express server
 const app = express();
@@ -15,25 +14,31 @@ const app = express();
 const mongoUrl = MONGODB_URI;
 mongoose.Promise = bluebird;
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
-    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
-).catch(err => {
-    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-    // process.exit();
-});
+mongoose
+    .connect(mongoUrl, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+    })
+    .catch(err => {
+        console.log(
+            'MongoDB connection error. Please make sure MongoDB is running. ' +
+                err
+        );
+        // process.exit();
+    });
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(
-    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
-);
 
 /**
  * Primary app routes.
  */
-app.get("/", homeController.getHome);
+app.get('/', homeController.getHome);
 
 export default app;
